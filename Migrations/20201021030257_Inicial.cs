4cd99c1e1a0d268a9fb1a379689f217c4663e147
Jhordy De Jesus Guzman Lantigua <jhordy_guzman@ucne.edu.do>
2020-10-20 23:04:55 -0400

    Creando Base de Datos

---
+++ Migrations/20201021030257_Inicial.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DROrdenes.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ordenes",
+                columns: table => new
+                {
+                    OrdenId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fecha = table.Column<DateTime>(nullable: false),
+                    Monto = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ordenes", x => x.OrdenId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrdenesDetalle",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    OrdenId = table.Column<int>(nullable: false),
+                    Cantidad = table.Column<float>(nullable: false),
+                    Costo = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrdenesDetalle", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrdenesDetalle_Ordenes_OrdenId",
+                        column: x => x.OrdenId,
+                        principalTable: "Ordenes",
+                        principalColumn: "OrdenId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrdenesDetalle_OrdenId",
+                table: "OrdenesDetalle",
+                column: "OrdenId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrdenesDetalle");
+
+            migrationBuilder.DropTable(
+                name: "Ordenes");
+        }
+    }
+}
